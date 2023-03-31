457d316be9b13d8ddd861cd5ba4ba6ddecd27e08
vuduynhiennn <vuduynhiencute@gmail.com>
2023-04-01 01:13:18 +0700

    I don't know what is going on

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Task3103 from './components/task3103';
+
+function App() {
+  return (
+    <div className="App">
+      <Task3103 />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/task3103/index.js
@@ -0,0 +1,62 @@
+import styles from "./task3103.module.scss"
+import data from "../../test3104_fake_data.json"
+import { useEffect, useState } from "react";
+
+const Task3103 = () => {
+    const [articles, setArticles] = useState([]);
+    const [article_highlight, setArticle_highlight] = useState([])
+    useEffect(() => {
+        // fetch data from server 
+        const article_highlight = data.filter((data) => {
+            return data.status === "highlight"
+        })
+
+        const articles_rest = data.filter((data => {
+            return data.status !== "highlight"
+        }))
+
+        setArticle_highlight(article_highlight[0])
+        setArticles(articles_rest)
+    }, []);
+
+    return (           
+        <div className={styles["articles_main"]}> 
+            {article_highlight && <div className={styles["article_highlight"]}>
+                <div className={styles["article_highlight_banner"]} 
+                     style={{backgroundImage: `url("${article_highlight.banner}")`}}>
+                    {console.log(article_highlight.banner)}
+                </div>  
+
+                <div className={styles["article_highlight_ctn"]}>
+                    <div className={styles["article_highlight_date"]}> {article_highlight.date} </div>
+                    <h1 className={`${styles.article_title} ${styles.article_highlight_title}`}> {article_highlight.title} </h1>
+                    <div className={styles["article_highlight_content"]}>{article_highlight.content}</div>
+                    <a className="readmore_button" href="localhost"> Xem tiếp...</a>
+                </div>
+            </div>}
+            
+            {articles && <div className={styles["articles_rest"]}>
+                {articles.map((article, i) => (
+                    <div className={styles["article_container"]} key={i}>
+                        <div className={styles["article_banner"]} 
+                            style={{backgroundImage: `url("${article.banner}")`}}>
+                        </div>
+                        <div className={styles["article_date"]}>
+                            {article.date}
+                        </div>
+
+                        <h1 className={styles["article_title"]}>
+                            {article.title} 
+                        </h1>
+
+                        <p className={styles["article_content"]}>
+                            {article.content}
+                        </p>
+                    </div>
+                ))}
+            </div>}
+        </div>
+    )
+}
+
+export default Task3103
